c9d8c3b56c7dbbe0938ac833e34ef402efc7febc
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-24 12:58:55 -0400

    Installed Sequalize, Express and added routes and middleware to Server.js.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const cors = require('cors');
+
+
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
